bbd623d37876c18f77b06d23d4d648055a05ada6
Raheem Mohamed <39874831+raheemmohamed@users.noreply.github.com>
2019-06-07 14:29:58 +0530

    Update dropdown.ts

--- src/app/components/dropdown/dropdown.ts
+++ src/app/components/dropdown/dropdown.ts
@@ -18,16 +18,29 @@ export const DROPDOWN_VALUE_ACCESSOR: any = {
 @Component({
     selector: 'p-dropdownItem',
     template: `
-        <li (click)="onOptionClick($event)" role="option"
-            [attr.aria-label]="option.label"
-            [ngStyle]="{'height': itemSize + 'px'}"
-            [ngClass]="{'ui-dropdown-item ui-corner-all':true,
-                                                'ui-state-highlight': selected,
-                                                'ui-state-disabled':(option.disabled),
-                                                'ui-dropdown-item-empty': !option.label||option.label.length === 0}">
-            <span *ngIf="!template">{{option.label||'empty'}}</span>
-            <ng-container *ngTemplateOutlet="template; context: {$implicit: option}"></ng-container>
-        </li>
+//         <li (click)="onOptionClick($event)" role="option"
+//             [attr.aria-label]="option.label"
+//             [ngStyle]="{'height': itemSize + 'px'}"
+//             [ngClass]="{'ui-dropdown-item ui-corner-all':true,
+//                                                 'ui-state-highlight': selected,
+//                                                 'ui-state-disabled':(option.disabled),
+//                                                 'ui-dropdown-item-empty': !option.label||option.label.length === 0}">
+//             <span *ngIf="!template">{{option.label||'empty'}}</span>
+//             <ng-container *ngTemplateOutlet="template; context: {$implicit: option}"></ng-container>
+//         </li>
+
+
+    <li (click)="onOptionClick($event)" role="option" 
+        [attr.aria-label]="option.label"
+        [ngStyle]="{'height': itemSize + 'px'}
+        [ngClass]="{'ui-dropdown-item ui-corner-all':true,
+        'ui-state-highlight': selected,
+        'ui-state-disabled':(option.disabled),
+        'ui-dropdown-item-empty': !option.label||option.label.length === 0}">        
+        <span *ngIf="!template" class="dd-label">{{option.label||'empty'}}</span>
+        <span  class="dd-value" *ngIf="!template">{{option.value}}</span>
+        <ng-container *ngTemplateOutlet="template; context: {$implicit: option}"></ng-container>
+    </li>"
     `
 })
 export class DropdownItem {
