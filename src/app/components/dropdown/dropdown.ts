12ecfc0a35c977cf41392f478902ddb4bff1677b
Raheem Mohamed <39874831+raheemmohamed@users.noreply.github.com>
2019-06-07 14:30:55 +0530

    new dropdown

--- src/app/components/dropdown/dropdown.ts
+++ src/app/components/dropdown/dropdown.ts
@@ -18,17 +18,6 @@ export const DROPDOWN_VALUE_ACCESSOR: any = {
 @Component({
     selector: 'p-dropdownItem',
     template: `
-//         <li (click)="onOptionClick($event)" role="option"
-//             [attr.aria-label]="option.label"
-//             [ngStyle]="{'height': itemSize + 'px'}"
-//             [ngClass]="{'ui-dropdown-item ui-corner-all':true,
-//                                                 'ui-state-highlight': selected,
-//                                                 'ui-state-disabled':(option.disabled),
-//                                                 'ui-dropdown-item-empty': !option.label||option.label.length === 0}">
-//             <span *ngIf="!template">{{option.label||'empty'}}</span>
-//             <ng-container *ngTemplateOutlet="template; context: {$implicit: option}"></ng-container>
-//         </li>
-
 
     <li (click)="onOptionClick($event)" role="option" 
         [attr.aria-label]="option.label"
